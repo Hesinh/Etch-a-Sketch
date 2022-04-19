fcc2df8df40d762910a9c11455bc72d24294f104
Agus Ibacache <chocobomj@gmail.com>
2022-04-19 19:04:06 -0300

    addded the random button,temporary fix to the structure

--- script.js
+++ script.js
@@ -5,15 +5,38 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
-    //cell.innerText = (i + 1);
     container.appendChild(cell).className = "grid-item";
     let btnErase = document.querySelector("#btnErase")
     btnErase.addEventListener('click' , function(){
       cell.style.backgroundColor= 'white';
     })
     cell.addEventListener('mouseover', function(e) {
-        e.target.style.backgroundColor = "orange";});
-    };
+        e.target.style.backgroundColor = "orange";}); // THIS is the default, i need to get the default OUT
+    
+    let selRandom = document.querySelector('#clrRandom');
+     selRandom.addEventListener('click', function() {
+        cell.addEventListener('mouseover', function(e){
+          e.target.style.backgroundColor = randColor();
+        })
+     })
+    //selRandom.addEventListener('click', function()  {
+      //cell.style.backgroundColor= randColor(); }):
+;};
 };
-makeRows(16, 16);
+let rows = 16;
+let cols = 16;
+makeRows(rows, cols);
 
+
+let changeRows = document.querySelector('#arrowChange');
+  changeRows.addEventListener('click', function(){
+     rows = prompt("enter the value of row");
+     cols = prompt("enter the value of columns")
+  });
+
+// This is a proof about rgb
+
+const randColor = () =>  {
+  return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
+document.querySelector('#header').style.backgroundColor = randColor();
