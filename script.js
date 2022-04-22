518923ef26b58244d8a0d0ec3561c10ab704493a
Agus Ibacache <chocobomj@gmail.com>
2022-04-21 22:06:26 -0300

    solved the size problem

--- script.js
+++ script.js
@@ -19,20 +19,39 @@ function makeRows(rows, cols) {
           e.target.style.backgroundColor = randColor();
         })
      })
-    //selRandom.addEventListener('click', function()  {
-      //cell.style.backgroundColor= randColor(); }):
+  };
 ;};
-};
-let rows = 16;
-let cols = 16;
-makeRows(rows, cols);
-
-
-let changeRows = document.querySelector('#arrowChange');
-  changeRows.addEventListener('click', function(){
-     rows = prompt("enter the value of row");
-     cols = prompt("enter the value of columns")
-  });
+makeRows(16,16);
+//This controls the button change , it allows you to create a new grid
+let changeGridSize = document.querySelector('#gridChange');
+changeGridSize.addEventListener('click' , function(){
+  let rowValue = prompt("Rows?");
+    if (rowValue > 0 ){
+    }else if(rowValue > 100) {
+      alert('100 is the max, try again')
+      return;
+    }else if(rowValue <0){
+      alert('negatives numbers are NOT allowed')
+      return;
+    }else {
+      alert('Try to put a number greater than 0')
+      return;
+    }
+let colValue = prompt("cols?");
+  if (colValue > 0 ){
+  }else if(colValue > 100) {
+    alert('100 is the max, try again')
+    return;
+  }else if(colValue <0){
+    alert('negatives numbers are NOT allowed')
+    return;
+  }else {
+    alert('Try to put a number greater than 0')
+    return;
+  }
+  document.getElementById('container').textContent = '';
+  makeRows(rowValue,colValue)
+});
 
 // This is a proof about rgb
 
@@ -40,3 +59,9 @@ const randColor = () =>  {
   return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
 document.querySelector('#header').style.backgroundColor = randColor();
+
+function reset () {
+  document.querySelectorAll('.grid-item').forEach(e => e.container.removeChild(e));
+}                              
+//let changeRows = document.querySelector('#arrowChange');
+//changeRows.addEventListener('click', function(){ });
